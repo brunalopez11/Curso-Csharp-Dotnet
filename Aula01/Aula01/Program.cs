7db88cc99336cba3f17ae53e202370fc7fdce513
brunalopez11 <brunalopez.421@gmail.com>
2026-03-15 14:04:41 -0300

    feat: lesson 01 - variables and basic data types

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula01;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Variaveis simples
+        string myName = "Bruna"; // string é um tipo de dado que armazena texto
+        int myAge = 20; // int é um tipo de dado que armazena números inteiros
+        char character = 'B'; // char é um tipo de dado que armazena um único caractere
+        double myHeight = 1.65; // double é um tipo de dado que armazena números com casas decimais
+        bool boolean = true; // bool é um tipo de dado que armazena valores booleanos (true ou false)
+        Console.WriteLine($"Meu nome é {myName}, tenho {myAge} anos, minha altura é {myHeight} metros e meu caractere favorito é '{character}'.");
+
+    }
+}
